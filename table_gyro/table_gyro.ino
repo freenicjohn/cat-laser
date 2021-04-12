70ee1e7575c32a8e427c2cc15fa8b99bebde59fe
LAPTOP-TM9IR910\Nic John <nic>
2021-04-12 10:48:05 -0500

    Made tilt control smoother by using cosine instead of linear control mapping.

--- table_gyro/table_gyro.ino
+++ table_gyro/table_gyro.ino
@@ -8,11 +8,11 @@
 // Initialize variables
 Servo servo_base;  // base axis
 Servo servo_tilt;  // tilt axis
-int x_reading = 0;
-int y_reading = 0;
-int tilt_command = 0;
+float x_reading = 0;
+float y_reading = 0;
+float tilt_command = 0.0;
 int base_command = 0;
-bool tilt_forward;
+float signal_max = 1023.0;
 float press_force = 0.0;
 
 // Set pin numbers
@@ -38,29 +38,27 @@ void setup() {
 
 void loop() {
   // read values from joystick
-  x_reading = map(analogRead(AI_X_PIN), 0, 1023, 50, -50);
-  y_reading = map(analogRead(AI_Y_PIN), 0, 1023, 50, -50);
+  x_reading = signal_max - analogRead(AI_X_PIN);  // raw signal 0 - 1023
+  y_reading = signal_max - analogRead(AI_Y_PIN);  // raw signal 0 - 1023
   Serial.print("X: ");
   Serial.print(x_reading);
   Serial.print(" | Y: ");
   Serial.println(y_reading);
 
-  // determine direction of tilt
-  tilt_forward = (y_reading > 0);
-
   // calculate angles for each axis
-  press_force = sqrt(pow(x_reading, 2) + pow(y_reading, 2));
-  if (tilt_forward){
-    tilt_command = map(press_force, 0, 50, 90, 180);
-    base_command = atan2(y_reading, x_reading) * 180 / M_PI;
-  }
-  else {
-    tilt_command = map(press_force, 0, 50, 90, 0);
-    base_command = 180 + atan2(y_reading, x_reading) * 180 / M_PI;
+  press_force = sqrt(pow(x_reading - (signal_max/2), 2) + pow(y_reading - (signal_max/2), 2));  // 0 - 50
+  tilt_command = cos(press_force / (signal_max / 2) * M_PI) * 90;
+  base_command = atan2((signal_max/2) - y_reading, (signal_max/2) - x_reading) * 180 / M_PI;
+  
+  if (y_reading > signal_max/2){
+    tilt_command = 180 - tilt_command ;
+    base_command = 180 + base_command;
   }
 
-  // send control to servos
-  servo_base.write(base_command);
+  // send control to servos - don't send if press force isn't over a threshold
+  if (press_force > 80){  
+    servo_base.write(base_command);
+  }
   servo_tilt.write(tilt_command);
   delay(15);  // waits for the servo to get there
 
