f84b7001926405dc16d46f167672a1f3c1d9e342
LAPTOP-TM9IR910\Nic John <nic@outlierautomation.com>
2021-05-10 22:31:07 -0500

    Successfully moving laser away from object - need to test with more consistent lighting.

--- cat_laser/Vision.py
+++ cat_laser/Vision.py
@@ -4,74 +4,84 @@ import cv2
 import numpy as np
 
 
-def get_webcam(width=640, height=480, brightness=100):
-    ''' Returns cap object of webcam stream'''
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # 0 uses default webcam
-    cap.set(3, width)  # set width - first param here is a setting ID
-    cap.set(4, height)  # set height
-    cap.set(10, brightness)  # set brightness
+class Vision:
+    def __init__(self, p1=152, p2=193):
+        """ Initialize the Vision object """
+        self.width = 640
+        self.height = 640
+        self.p1 = p1
+        self.p2 = p2
+        self.img = np.zeros(1)
+        self.img_warped = np.zeros(1)
+        self.img_dilated = np.zeros(1)
+        self.img_contour = np.zeros(1)
+        self.brightness = 100
+        self.contour_threshold = 5000
+        self.cX = 0
+        self.cY = 0
 
-    return cap
+        # Initialize webcam capture
+        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # 0 uses default webcam
+        self.cap.set(3, self.width)  # set width - first param here is a setting ID
+        self.cap.set(4, self.height)  # set height
+        self.cap.set(10, self.brightness)  # set brightness
 
-
-def preProcessing(img, debug=False):
-    if debug:
-        # parameters to tune
-        p_1 = cv2.getTrackbarPos("P 1", "TrackBars")
-        p_2 = cv2.getTrackbarPos("P 2", "TrackBars")
-    else:
-        # Values from tuning
-        p_1 = 152
-        p_2 = 193
-
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 2)
-    imgCanny = cv2.Canny(imgBlur, p_1, p_2)  # 1 and 2 are threshold params
-    kernel = np.ones((5, 5), np.uint8)  # kernel required for dilation function
-    imgDial = cv2.dilate(imgCanny, kernel, iterations=10)  # dilation makes edges thicker
-
-    imgStack = stack_images(0.8, ([[imgGray, imgDial]]))
-
-    if debug:
-        cv2.imshow("Test", imgStack)
-
-    return imgDial
-
-
-def getContours(img, original_img, shape_rec=False):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    maxArea = 0
-    biggest = np.array([])
-    imgContour = original_img.copy()
-    x, y, w, h = 0, 0, 0, 0
-
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 5000:  # Using a threshold to decide if it's worth drawing the contour
-            peri = cv2.arcLength(cnt, True)  # Get perimeter of each contour
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)  # Get corner points for each contour
-            if area > maxArea:
-                best_cnt = cnt
-                biggest = approx
-                maxArea = area
-                x, y, w, h = cv2.boundingRect(approx)  # draw bounding box around contour
-                cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 2)
+    def pre_processing(self):
+        """ Creates dilated image ready for contouring """
+        if __name__ == '__main__':
+            # parameters to tune
+            p_1 = cv2.getTrackbarPos("P 1", "TrackBars")
+            p_2 = cv2.getTrackbarPos("P 2", "TrackBars")
         else:
-            best_cnt = cnt
+            # Values from tuning
+            p_1 = self.p1
+            p_2 = self.p2
 
-    # Calculate center of biggest contour
-    if len(contours) > 0:
-        M = cv2.moments(best_cnt)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        # draw the center of the shape on the image
-        cv2.circle(imgContour, (cX, cY), 7, (255, 255, 255), -1)
+        img_gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.GaussianBlur(img_gray, (5, 5), 2)
+        img_canny = cv2.Canny(img_blur, p_1, p_2)  # 1 and 2 are threshold params
+        kernel = np.ones((5, 5), np.uint8)  # kernel required for dilation function
+        self.img_dilated = cv2.dilate(img_canny, kernel, iterations=10)  # dilation makes edges thicker
 
-    return imgContour, biggest
+    def get_contours(self):
+        contours, hierarchy = cv2.findContours(self.img_dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        max_area = 0
+        best_cnt = []
+        self.img_contour = self.img_warped.copy()
+
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > self.contour_threshold:  # Using a threshold to decide if it's worth drawing the contour
+                if area > max_area:
+                    # Found new largest contour
+                    best_cnt = cnt
+                    max_area = area
+                    cv2.drawContours(self.img_contour, cnt, -1, (255, 0, 0), 2)
+
+        if len(contours) > 0:
+            # Calculate center of biggest contour
+            M = cv2.moments(best_cnt)
+            self.cX = int(M["m10"] / M["m00"])
+            self.cY = int(M["m01"] / M["m00"])
+            # draw the center of the shape on the image
+            cv2.circle(self.img_contour, (self.cX, self.cY), 7, (255, 255, 255), -1)
+
+    def get_center_of_object(self):
+        """ Draws a blue line around every object larger than the threshold area and a white dot at the center
+            of the largest object
+            Returns the center point coordinates of the largest object in an image
+        """
+        success, self.img = self.cap.read()  # Read from the webcam
+        self.img = cv2.resize(self.img, (self.width, self.height))
+        self.img_warped = perspective_transform(self.img, self.width, self.height)
+        self.pre_processing()
+        self.get_contours()
+
+        return self.cX, self.cY
 
 
 def perspective_transform(img, width, height):
-
+    """ Warps an angled image to create flat 2d image"""
     pts1 = np.float32([[173, 205], [536, 220], [67, 487], [621, 497]])  #  the 4 corners of the shape - can be gotten
                                                                         #  from paint or ShareX (cursor locations at
                                                                         #  the corners)
@@ -82,7 +92,10 @@ def perspective_transform(img, width, height):
     return img_warped
 
 
-def stack_images(scale, img_array):  # Function written by murtaza (YouTube) to stack images (I re-factored var names)
+def stack_images(scale, img_array):
+    """ Helper function to stack images together for display
+        Written by murtaza (YouTube) (I only re-factored variable names)
+    """
     rows = len(img_array)
     cols = len(img_array[0])
     rows_available = isinstance(img_array[0], list)
@@ -117,37 +130,22 @@ def stack_images(scale, img_array):  # Function written by murtaza (YouTube) to 
 
 
 if __name__ == '__main__':
-    debug = False
+    camera = Vision()
 
-    # Frame size
-    width_img = 640
-    height_img = 640
-
-    if debug:
-        # Create trackbar for tuning
-        def empty(a):
-            pass
-
-        cv2.namedWindow("TrackBars")
-        cv2.resizeWindow("TrackBars", 640, 240)
-        cv2.createTrackbar("P 1", "TrackBars", 0, 200, empty)
-        cv2.createTrackbar("P 2", "TrackBars", 0, 200, empty)
-
-        # parameters to tune
-        p_1 = cv2.getTrackbarPos("P 1", "TrackBars")
-        p_2 = cv2.getTrackbarPos("P 2", "TrackBars")
-
-    cap = get_webcam(width_img, height_img)  # Get capture from webcam
+    # Create trackbars for contour recognition tuning
+    def empty(a):
+        pass
+    cv2.namedWindow("TrackBars")
+    cv2.resizeWindow("TrackBars", 640, 240)
+    cv2.createTrackbar("P 1", "TrackBars", camera.p1, 200, empty)
+    cv2.createTrackbar("P 2", "TrackBars", camera.p2, 200, empty)
 
     while True:
-        success, img = cap.read()  # Read from the webcam
-        img = cv2.resize(img, (width_img, height_img))
-        imgWarped = perspective_transform(img, width_img, height_img)
-        imgThresh = preProcessing(imgWarped, debug)
-        imgContour, biggest = getContours(imgThresh, imgWarped)
-
-        stackedImages = stack_images(0.6, [img, imgWarped, imgContour])
-
+        x, y = camera.get_center_of_object()
+        stackedImages = stack_images(0.6, [camera.img, camera.img_warped, camera.img_contour])
         cv2.imshow("Result", stackedImages)
+
+        print(x, y)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- cat_laser/cat_laser.py
+++ cat_laser/cat_laser.py
@@ -1,5 +1,51 @@
 # Program to move a laser around on the ground to entertain a cat.
 # Written by Nic John 2021.
 
+from Turret import *
+from Vision import *
+import time
+
+
+def convert_coords_for_laser(x, y):
+    max = 640
+    min_x_out = 49
+    min_y_out = 45
+    max_x_out = 125
+    max_y_out = 65
+
+    x_range_out = max_x_out - min_x_out
+    y_range_out = max_y_out - min_y_out
+
+    percent_x = x/max
+    percent_y = y/max
+
+    base = min_x_out + (percent_x * x_range_out)
+    tilt = min_y_out + (percent_y * y_range_out)
+
+    return base, tilt
+
+
 if __name__ == '__main__':
-    pass
+    camera = Vision()
+    laser_turret = Turret()
+    laser = "1"
+
+    while True:
+        center = camera.get_center_of_object()
+        stackedImages = stack_images(0.6, [camera.img, camera.img_warped, camera.img_contour])
+
+        base, tilt = convert_coords_for_laser(center[0], center[1])
+
+        laser_turret.command(base, tilt, laser)
+
+        # Debugging visualization
+        cv2.imshow("Result", stackedImages)
+        print("Target: (%s, %s)" % (center[0], center[1]))
+        print("Base: %s, Tilt: %s" % (base, tilt))
+
+        time.sleep(0.1)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    laser.command(90, 90, 0)  # Turn turret off
